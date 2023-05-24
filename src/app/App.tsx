6c1b8acf0d411b56f600c0b576254dc47442c40b
Julia <bela@d-element.ru>
2023-05-24 10:32:36 +0500

    end module 2, lesson 15

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,15 +5,6 @@ import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
 import {Suspense} from "react";
-import {useTranslation} from "react-i18next";
-
-function Component() {
-    const { t, i18n } = useTranslation();
-
-    return <div>
-        {t('Тестовый перевод')}
-    </div>
-}
 
 const App = () => {
     const { theme } = useTheme();
@@ -22,7 +13,6 @@ const App = () => {
         <div className={classNames("app", {}, [theme])}>
             <Suspense fallback="" >
                 <Navbar />
-                <Component />
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
